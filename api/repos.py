45c67eae778bbf2a482ac89aa90787389577f8ce
chenkexing <1585799755@qq.com>
2023-06-30 18:38:06 +0800

    mv source to datas

--- api/repos.py
+++ api/repos.py
@@ -2,7 +2,7 @@
 from flask import Blueprint
 from flask import jsonify
 
-from sources.metrics import Test, Test_schema
+from datas.metrics import Test, Test_schema
 
 repos_bp = Blueprint('repos', __name__)
 
@@ -17,5 +17,11 @@ def list():
 
 @repos_bp.route('/test')
 def remote_test():
+    test = Test.query.all()
+    return Test_schema.dump(test)
+
+
+@repos_bp.route('/sloc')
+def sloc():
     test = Test.query.all()
     return Test_schema.dump(test)
