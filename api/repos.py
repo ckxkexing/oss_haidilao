1fbc8776a4069af11f34bc7e40e2b6d228aa8eb3
chenkexing <1585799755@qq.com>
2023-06-30 19:41:39 +0800

    fix bug of primary key in flask sqlAlchemy

--- api/repos.py
+++ api/repos.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from flask import jsonify
 
 from datas.metrics import Test, Test_schema
+from datas.models import Sloc, Sloc_schema
 
 repos_bp = Blueprint('repos', __name__)
 
@@ -21,7 +22,9 @@ def remote_test():
     return Test_schema.dump(test)
 
 
-@repos_bp.route('/sloc')
-def sloc():
-    test = Test.query.all()
-    return Test_schema.dump(test)
+@repos_bp.route('/sloc/')
+@repos_bp.route('/sloc/<int:page>')
+def list_sloc(page=1):
+    per_page = 10
+    info = Sloc.query.paginate(page=page, per_page=per_page, error_out=False)
+    return Sloc_schema.dump(info)
--- datas/models.py
+++ datas/models.py
@@ -2,6 +2,7 @@ from .database import db, ma
 
 class db_config(object):
     bind_name = "models"
+
     account = 'root'  # 账号
     password = 'root'  # 填入密码
     proname = 'core_developers'  # 填入数据库名
@@ -32,3 +33,19 @@ Developers_schema = DevelopersSchema(many=True)
 ###
 #   repos table
 ###
+
+# sloc
+class Sloc(db.Model):
+    __tablename__ = 'sloc'
+    __bind_key__ = db_config.bind_name
+    owner:str = db.Column(db.String(255))
+    repo:str = db.Column(db.String(255))
+    commit:str = db.Column(db.String(255), primary_key=True)
+    sloc:int = db.Column(db.Integer)
+
+class SlocSchema(ma.Schema):
+    class Meta:
+        fields = ("owner", "repo", "commit", "sloc")
+
+Sloc_schema = SlocSchema(many=True)
+
