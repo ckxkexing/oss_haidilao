afb0831d9b9f09bac824ec24381055897b38b5ad
malin <malin3598@foxmail.com>
2023-07-18 15:08:18 +0800

    添加多个指标接口

---
+++ api/centrality_score.py
@@ -0,0 +1,266 @@
+# -*-coding:utf-8-*-
+import json
+
+from flask import Blueprint
+
+from datas.models import ContributedReposRole, ContributedReposRoleSchema, contributed_repos_role_schema, \
+    TotalFixIntensity, TotalFixIntensitySchema, total_fix_intensity_role_schema, ContributionGraph, \
+    ContributionGraphSchema, contribution_graph_data_schema, PeersAverageFixIntensity, PeersAverageFixIntensitySchema, \
+    peers_average_fix_intensity_role_schema, DeveloperRolesMetrics, DeveloperRolesMetricsSchema, \
+    developer_roles_metrics_schema
+from helpers.clickhouse_cli import CKClient
+
+centrality_score_bp = Blueprint('centrality_score', __name__)
+
+
+@centrality_score_bp.route('/metric/contributed_repos')
+def get_contributed_repos():
+    score = ContributedReposRole.query.all()
+    contributed_repos = ContributedReposRoleSchema.dump(contributed_repos_role_schema, score)
+    map_list = []
+    for data in contributed_repos:
+        map_list.append({"email": data["author_email"], "maximum_join_in_repo": data["repo_count"]})
+    return map_list
+
+
+@centrality_score_bp.route('/metric/total_fix_intensity')
+def get_total_fix_intensity():
+    score = TotalFixIntensity.query.all()
+    total_fix_intensity = TotalFixIntensitySchema.dump(total_fix_intensity_role_schema, score)
+    map_list = []
+    for data in total_fix_intensity:
+        map_list.append({"email": data["author_email"], "total_fix_intensity": data["total_fix_intensity"]})
+    return map_list
+
+
+@centrality_score_bp.route('/metric/basic_graph')
+def get_basic_graph():
+    response = {}
+    score = ContributionGraph.query.all()
+    contribution_graph = ContributionGraphSchema.dump(contribution_graph_data_schema, score)
+    results = []
+    for data in contribution_graph:
+        results.append({"repo_name": data["repo_name"], "cntrb_id": data["cntrb_login"],
+                        "total_contributions": data["total_contributions"]})
+    repo_node = []
+    person_node = []
+    edges = []
+    for result in results:
+        if result["total_contributions"] > 1:
+            repo_node.append(result["repo_name"])
+            person_node.append(result["cntrb_id"])
+            edges.append(
+                {"source": result["repo_name"], "target": result["cntrb_id"],
+                 "value": result["total_contributions"] / 2000})
+    repo_node_set = set(repo_node)
+    person_node_set = set(person_node)
+    nodes = []
+    for node in repo_node_set:
+        nodes.append({"id": node, "size": 50, "label": node})
+    for node in person_node_set:
+        nodes.append({"id": node, "size": 10})
+    response["nodes"] = nodes
+    response["edges"] = edges
+    return response
+
+
+@centrality_score_bp.route('/metric/peers_average_fix_intensity')
+def get_peers_average_fix_intensity():
+    score = PeersAverageFixIntensity.query.all()
+    peers_average_fix_intensity = PeersAverageFixIntensitySchema.dump(peers_average_fix_intensity_role_schema, score)
+    return peers_average_fix_intensity
+
+
+
+
+
+@centrality_score_bp.route('/metric/person_metrics')
+def get_person_metrics():
+    score = DeveloperRolesMetrics.query.all()
+    developer_roles_metrics = DeveloperRolesMetricsSchema.dump(developer_roles_metrics_schema, score)
+    map_list = []
+    for result in developer_roles_metrics:
+        map_list.append(
+            {"email": result["author_email"],
+             "total_fix_commit_count": result["total_fix_commit_count"],
+             "maximum_fix_commit_count": result["maximum_fix_commit_count"],
+             "fist_year_joined_repo_count": result["repo_count"]})
+    return map_list
+
+# @centrality_score_bp.route('/metric/get_betweenness_centtality')
+# def get_betweenness_centtality():
+#     basic_graph = get_basic_graph()
+#     with open('../merged_df.json', 'r') as f:
+#         results = json.load(f)
+#         repo_node = []
+#         for result in results:
+#             if result["total_contributions"] > 1:
+#                 repo_node.append(result["repo_name"])
+#     repo_node_set = set(repo_node)
+#     with open('../betweenness_centrality.json', 'r') as f:
+#         nodes = []
+#         results = json.load(f)
+#         for result in results:
+#             if result in repo_node_set:
+#                 nodes.append({"id": result, "size": results[result] * 200, "label": result})
+#             else:
+#                 if results[result] == 0:
+#                     nodes.append({"id": result, "size": 1})
+#                 else:
+#
+#                     nodes.append({"id": result, "size": results[result] * 5})
+#     basic_graph["nodes"] = nodes
+#     # print(basic_graph)
+#     return basic_graph
+
+
+# @centrality_score_bp.route('/metric/get_maximum_fix_intensity')
+# def get_maximum_fix_intensity():
+#     ck = CKClient(
+#
+#     )
+#     results = ck.execute_no_params("""
+#     select *
+# from (select author_email, search_key__owner, search_key__repo, count() as commit_count, ln(commit_count + 1)
+#       from (select search_key__owner, search_key__repo, author_email, authored_date, b.*
+#             from (select *
+#                   from gits
+#                   where search_key__owner = 'rust-lang'
+#                     and search_key__repo != 'llvm-project'
+#                     and search_key__repo != 'llvm'
+#                     and search_key__repo != 'rust-gha'
+#                     and length(parents) == 1
+#                     and author_email != '') as a global ASOF
+#                      INNER JOIN (select author_email,
+#                                         min(authored_date)          as start_at,
+#                                         subtractYears(start_at, -1) as end_at
+#                                  from gits
+#                                  where search_key__owner = 'rust-lang'
+#                                    and search_key__repo != 'llvm-project'
+#                                    and search_key__repo != 'llvm'
+#                                    and search_key__repo != 'rust-gha'
+#                                    and length(parents) == 1
+#                                    and author_email != ''
+#                                  group by author_email) as b
+#                                 on a.author_email = b.author_email and a.authored_date <= end_at)
+#       group by author_email, search_key__owner, search_key__repo
+#       order by commit_count desc)
+# limit 1 by author_email
+#
+#     """)
+#     map_list = []
+#     for result in results:
+#         # print(result)
+#         if result[3] > 1:
+#             map_list.append({"email": result[0], "maximum_fix_intensity": result[3]})
+#     ck.close()
+#     return map_list
+
+
+
+
+
+
+
+
+# @centrality_score_bp.route('/metric/get_basic_graph')
+# def get_basic_graph():
+#     response = {}
+#     with open('../merged_df.json', 'r') as f:
+#         results = json.load(f)
+#         repo_node = []
+#         person_node = []
+#         edges = []
+#         for result in results:
+#             if result["total_contributions"] > 1:
+#                 repo_node.append(result["repo_name"])
+#                 person_node.append(result["cntrb_id"])
+#                 edges.append(
+#                     {"source": result["repo_name"], "target": result["cntrb_id"],
+#                      "value": result["total_contributions"] / 2000})
+#         repo_node_set = set(repo_node)
+#         person_node_set = set(person_node)
+#         nodes = []
+#         for node in repo_node_set:
+#             nodes.append({"id": node, "size": 50, "label": node})
+#         for node in person_node_set:
+#             nodes.append({"id": node, "size": 10})
+#         response["nodes"] = nodes
+#         response["edges"] = edges
+#     return response
+
+
+@centrality_score_bp.route('/metric/get_page_rank_top_10')
+def get_page_rank_top_10():
+    with open('../pagerank10.json', 'r') as f:
+        nodes = []
+        results = json.load(f)
+        for result in results:
+            nodes.append({"id": result, "size": results[result] * 2000, "label": result})
+    return {"nodes": nodes}
+
+
+# @centrality_score_bp.route('/metric/get_betweenness_centtality')
+# def get_betweenness_centtality():
+#     basic_graph = get_basic_graph()
+#
+#     with open('../merged_df.json', 'r') as f:
+#         results = json.load(f)
+#         repo_node = []
+#         for result in results:
+#             if result["total_contributions"] > 1:
+#                 repo_node.append(result["repo_name"])
+#     repo_node_set = set(repo_node)
+#     with open('../betweenness_centrality.json', 'r') as f:
+#         nodes = []
+#         results = json.load(f)
+#         for result in results:
+#             if result in repo_node_set:
+#                 nodes.append({"id": result, "size": results[result] * 200, "label": result})
+#             else:
+#                 if results[result] == 0:
+#                     nodes.append({"id": result, "size": 1})
+#                 else:
+#
+#                     nodes.append({"id": result, "size": results[result] * 5})
+#     basic_graph["nodes"] = nodes
+#     # print(basic_graph)
+#     return basic_graph
+
+
+# @centrality_score_bp.route('/sendSql', methods=['POST'])
+# def sendSql():
+#     sql_ = request.data.decode("utf8")
+#     results = ck.execute_no_params(sql_)
+#     response = []
+#     for result in results:
+#         response.append({"value":result[0],"label":result[0]})
+#     return response
+
+
+
+# @centrality_score_bp.route('/metric/get_centrality_score')
+# def get_centrality_score():
+#     # 连接数据库
+#     score = CentralityScore.query.all()
+    return CentralityScoreSchema.dump(score)
+    # results = db_engine.execute("select * from centrality_score").fetchall()
+    # # 创建游标
+    # # 执行 SQL 查询
+    # key = 0
+    # response = []
+    # for result in results:
+    #     key += 1
+    #     # print(result)
+    #     response.append({
+    #
+    #         "key": key,
+    #         "repo": result[1],
+    #         "page_rank": result[2],
+    #         "betweenness_centrality": result[3],
+    #         "closeness_centrality": result[4],
+    #         "total_score": result[5]
+    #
+    #     })
+    #
+    # return response
--- datas/models.py
+++ datas/models.py
@@ -1,17 +1,19 @@
 from .database import db, ma
+from .metrics_config import db_config
 
-class db_config(object):
-    bind_name = "models"
 
-    account = 'root'  # 账号
-    password = 'root'  # 填入密码
-    proname = 'core_developers'  # 填入数据库名
-    ip = 'mysql'  # 填入服务器网址
-    port = '3306'  # 端口
-    SECRET_KEY = "hahahaha"  # session密钥hhhhhhhh
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(account, password, ip, port, proname)
-    # print(SQLALCHEMY_DATABASE_URI)
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+# class db_config(object):
+#     bind_name = "models"
+#
+#     account = 'root'  # 账号
+#     password = 'root'  # 填入密码
+#     proname = 'core_developers'  # 填入数据库名
+#     ip = ''  # 填入服务器网址
+#     port = '3306'  # 端口
+#     SECRET_KEY = "hahahaha"  # session密钥hhhhhhhh
+#     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(account, password, ip, port, proname)
+#     # print(SQLALCHEMY_DATABASE_URI)
+#     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 ###
@@ -30,6 +32,7 @@ class DevelopersSchema(ma.Schema):
 
 Developers_schema = DevelopersSchema(many=True)
 
+
 ###
 #   repos table
 ###
@@ -43,9 +46,113 @@ class Sloc(db.Model):
     commit:str = db.Column(db.String(255), primary_key=True)
     sloc:int = db.Column(db.Integer)
 
+
 class SlocSchema(ma.Schema):
     class Meta:
         fields = ("owner", "repo", "commit", "sloc")
 
+
 Sloc_schema = SlocSchema(many=True)
 
+
+class CentralityScore(db.Model):
+    __tablename__ = 'centrality_score'
+    __bind_key__ = db_config.bind_name
+    id: int = db.Column(db.Integer, primary_key=True)
+    repo: str = db.Column(db.String(255))
+    page_rank: float = db.Column(db.Float)
+    betweenness_centrality: float = db.Column(db.Float)
+    closeness_centrality: float = db.Column(db.Float)
+    total_score: float = db.Column(db.Float)
+
+
+class CentralityScoreSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "repo", "page_rank", "betweenness_centrality", "closeness_centrality", "total_score")
+
+
+centrality_score_schema = CentralityScoreSchema(many=True)
+
+
+class ContributedReposRole(db.Model):
+    __tablename__ = 'contributed_repos_role'
+    __bind_key__ = db_config.bind_name
+    id: int = db.Column(db.Integer, primary_key=True)
+    author_email: str = db.Column(db.String(255))
+    repo_count: int = db.Column(db.Integer)
+
+
+class ContributedReposRoleSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "author_email", "repo_count")
+
+
+contributed_repos_role_schema = ContributedReposRoleSchema(many=True)
+
+
+class TotalFixIntensity(db.Model):
+    __tablename__ = 'total_fix_intensity'
+    __bind_key__ = db_config.bind_name
+    id: int = db.Column(db.Integer, primary_key=True)
+    author_email: str = db.Column(db.String(255))
+    total_fix_intensity: int = db.Column(db.Integer)
+
+
+class TotalFixIntensitySchema(ma.Schema):
+    class Meta:
+        fields = ("id", "author_email", "total_fix_intensity")
+
+
+total_fix_intensity_role_schema = TotalFixIntensitySchema(many=True)
+
+
+class ContributionGraph(db.Model):
+    __tablename__ = 'contribution_graph_data'
+    __bind_key__ = db_config.bind_name
+    id: int = db.Column(db.Integer, primary_key=True)
+    cntrb_login: str = db.Column(db.String(255))
+    repo_name: str = db.Column(db.String(255))
+    total_contributions: int = db.Column(db.Integer)
+
+
+class ContributionGraphSchema(ma.Schema):
+    class Meta:
+        fields = ("cntrb_login", "repo_name", "total_contributions")
+
+
+contribution_graph_data_schema = ContributionGraphSchema(many=True)
+
+
+class PeersAverageFixIntensity(db.Model):
+    __tablename__ = 'peers_average_fix_intensity_role'
+    __bind_key__ = db_config.bind_name
+    id: int = db.Column(db.Integer, primary_key=True)
+    repo: str = db.Column(db.String(255))
+    author_email: str = db.Column(db.String(255))
+    average_fix_intensity: float = db.Column(db.Float)
+
+
+class PeersAverageFixIntensitySchema(ma.Schema):
+    class Meta:
+        fields = ("repo", "author_email", "average_fix_intensity")
+
+
+peers_average_fix_intensity_role_schema = PeersAverageFixIntensitySchema(many=True)
+
+
+class DeveloperRolesMetrics(db.Model):
+    __tablename__ = 'developer_roles_metrics'
+    __bind_key__ = db_config.bind_name
+    id: int = db.Column(db.Integer, primary_key=True)
+    author_email: str = db.Column(db.String(255))
+    total_fix_commit_count: int = db.Column(db.Integer)
+    maximum_fix_commit_count: int = db.Column(db.Integer)
+    repo_count: int = db.Column(db.Integer)
+
+
+class DeveloperRolesMetricsSchema(ma.Schema):
+    class Meta:
+        fields = ("author_email", "total_fix_commit_count", "maximum_fix_commit_count", "repo_count")
+
+
+developer_roles_metrics_schema = DeveloperRolesMetricsSchema(many=True)
