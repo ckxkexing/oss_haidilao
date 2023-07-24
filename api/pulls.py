eb5a9e1a236d3feb18ef8804401848ad1773903e
chenkexing <1585799755@qq.com>
2023-07-24 13:09:09 +0800

    now can use raw sql on clickhouse data

--- api/pulls.py
+++ api/pulls.py
@@ -1,25 +1,73 @@
 from flask import Blueprint
 from flask import jsonify
+from sqlalchemy import text
 
 from models.ck import GithubPullRequests, github_pull_requests_schema
+from models.ck import execute_raw_sql
 
 pulls_bp = Blueprint('pulls', __name__)
 
+########################################
+### list pull requests
+########################################
 
 @pulls_bp.route('/')
 @pulls_bp.route('/<int:page>')
-def list_sloc(page=1):
+def list_pulls(page=1):
     per_page = 10
     info = GithubPullRequests.query.paginate(page=page, per_page=per_page, error_out=False)
     return github_pull_requests_schema.dump(info)
 
+@pulls_bp.route('/list/<owner>/<repo>')
+@pulls_bp.route('/list/<owner>/<repo>/<int:page>')
+def list_repo_pulls(owner, repo, page=1):
+    per_page = 10
+    info = GithubPullRequests.query.filter(
+                GithubPullRequests.search_key__owner==owner, 
+                GithubPullRequests.search_key__repo==repo
+        ).paginate(page=page, per_page=per_page, error_out=False)
+    return github_pull_requests_schema.dump(info)
+
+########################################
+### project pulls current state
+########################################
 
 @pulls_bp.route('/count/<owner>/<repo>')
 def cnt_pull_requests(owner, repo):
-    info = GithubPullRequests.query.filter(
+    # 
+    pulls_count = GithubPullRequests.query.filter(
                 GithubPullRequests.search_key__owner==owner, 
                 GithubPullRequests.search_key__repo==repo
             ).count()
 
-    res = {"owner": owner, "repo" :repo, "pulls_count": info}
+    res = {"owner": owner, "repo" :repo, "pulls_count": pulls_count}
+    return jsonify(res)
+
+########################################
+### pull_request state
+########################################
+
+@pulls_bp.route('/detail/<owner>/<repo>/<number>')
+def pull_request_features_at_open(owner, repo, number):
+    res = {}
+    #
+    # Project Dimension
+
+    ret = execute_raw_sql(text("""
+                select count(gpr.id) as open_pr_num from github_pull_requests as gpr
+                where gpr.search_key__owner=:owner and gpr.search_key__repo=:repo
+                    and gpr.closed_at = NULL
+            """), {"owner":owner, "repo":repo})
+    open_pr_num = ret.first()['open_pr_num']
+    res["open_pr_num"] = open_pr_num
+
+
+    #
+    # Pull Request Dimension
+
+
+    #
+    # Owner Dimension
+
+
     return jsonify(res)
--- models/ck.py
+++ models/ck.py
@@ -3,10 +3,20 @@ from .config import ck_db_config as db_config
 
 from sqlalchemy import String, Boolean, Integer, DateTime
 
+### session for raw sql execute
+def execute_raw_sql(query, params):
+    res = db.session.execute(query, params,
+                          bind=db.get_engine(db_config.bind_name))
+    # return dict
+    return res.mappings()
+
+### TABLES
+
 class GithubPullRequests(db.Model):
     __tablename__ = 'github_pull_requests'
     __bind_key__ = db_config.bind_name
-    id: int = db.Column(db.Integer, primary_key=True)
+    id: int = db.Column(Integer, primary_key=True)
+    number: int = db.Column(Integer)
     url: str = db.Column(String)
     html_url: str = db.Column(String)
     created_at = db.Column(DateTime)
@@ -17,6 +27,6 @@ class GithubPullRequests(db.Model):
 class GithubPullRequestsSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ("id", "url", "html_url", "created_at", "search_key__owner", "search_key__repo")
+        fields = ("id", "number", "url", "html_url", "created_at", "search_key__owner", "search_key__repo")
 
 github_pull_requests_schema = GithubPullRequestsSchema(many=True)
