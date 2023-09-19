b579409e291e6922bb56abe7c1784a3ee800b811
chenkexing <1585799755@qq.com>
2023-09-19 17:12:45 +0800

    update config

--- models/config.py
+++ models/config.py
@@ -1,6 +1,8 @@
 import os
+from dotenv import load_dotenv
 
 class metrics_db_config(object):
+    load_dotenv()
     bind_name = "metrics" 
     SQLALCHEMY_DATABASE_URI = os.environ['SQL_URL']
     print(SQLALCHEMY_DATABASE_URI)
